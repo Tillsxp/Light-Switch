91febed8dd811c339a2dae3c4492de39e2fd5486
Tillsxp <matilda.89@live.se>
2023-06-01 17:25:01 +0200

    background color change

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const on = document.getElementsByClassName('on')[0];
 const off = document.getElementsByClassName('off-bottom')[0];
+const background = document.querySelector('body');
 
 function toggleButton() {
   on.classList.toggle('off-top');
   on.classList.toggle('on');
   off.classList.toggle('on');
   off.classList.toggle('off-bottom');
+
+  if (background.style.backgroundColor === 'skyblue') {
+    background.style.backgroundColor = 'grey';
+    background.style.opacity = '0.2';
+  } else {
+    background.style.backgroundColor = 'skyblue';
+    background.style.opacity = '1';
+  }
 }
+
 on.addEventListener('click', function () {
   toggleButton();
 });
